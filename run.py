6965a7388f3d942e57f17057a39f016c5301972b
Gabriel S <gabriel.siket@gmail.com>
2022-06-24 10:58:44 +0000

    add while loop to check user input and computer pick

--- run.py
+++ run.py
@@ -2,3 +2,24 @@ import random
 
 user_score = 0
 computer_score = 0
+
+selection = ["rock", "paper", "scissors"]
+
+while True:
+    user_input = input("Make a choice: Rock, Paper, Scissors or Quit?: ").lower()
+    if user_input == "quit":
+        print("Sorry to see you go!")
+        quit()
+
+    if user_input not in selection:
+        print("Please make a valid selection!")
+        continue
+    
+    random_num = random.randint(0, 2)
+    # rock is 0, paper is 1 and scissors is 2
+    computer_pick = selection[random_num]
+    print("Computer picked", computer_pick + "!")
+
+
+print("Thanks for playing!")
+
